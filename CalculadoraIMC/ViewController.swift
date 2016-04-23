c8129a069c05480b8003e8d7e7e06d02c797559c
José Luis de la Torre Pulido <Jose@imac-de-jose.local>
2016-04-23 17:20:30 +0200

    First version

--- CalculadoraIMC/ViewController.swift
+++ CalculadoraIMC/ViewController.swift
@@ -8,16 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var peso: UITextField!
+    @IBOutlet weak var altura: UITextField!
+    @IBOutlet weak var scroll: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        peso.delegate=self
+        altura.delegate=self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func textFieldDidBeginEditing(textField: UITextField) {
+        var punto : CGPoint
+        punto = CGPointMake(0, textField.frame.origin.y - 50)
+        self.scroll.setContentOffset(punto, animated: true)
+    }
+    
+    @IBAction func textFieldDidEndEditing(textField: UITextField) {
+        self.scroll.setContentOffset(CGPointZero, animated: true)
+    }
+    
+    @IBAction func calcular(sender: AnyObject) {
+        var IMC : Double
+        let pesoLocal : Double
+        pesoLocal = Double (self.peso.text!)!
+        let estaturaLocal : Double = Double(self.altura.text!)!
+        IMC = pesoLocal/(estaturaLocal*estaturaLocal)
+        print("Tu IMC:\(IMC)")
+    }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder() // ocultar teclado
+    }
+    
+    @IBAction func bgTap(sender: UIControl){
+        peso.resignFirstResponder()
+        altura.resignFirstResponder()
     }
 
 
