b10079d570885a0f23fc5c80177d7f3784e661db
José Luis de la Torre Pulido <Jose@imac-de-jose.local>
2016-04-23 16:11:20 +0200

    Initial Commit

---
+++ CalculadoraIMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculadoraIMC
+//
+//  Created by José Luis de la Torre Pulido on 23/4/16.
+//  Copyright © 2016 José Luis de la Torre Pulido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
